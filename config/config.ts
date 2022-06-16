6765ecda1e16c8a61a3d6935579a0c4b08c06fca
mfuu <446310957@qq.com>
2022-06-16 10:52:29 +0800

    Update config

--- config/config.ts
+++ config/config.ts
@@ -2,25 +2,26 @@ import { defineConfig } from 'umi';
 import routes from './routes';
 
 export default defineConfig({
-  antd: {},
-  dva: {},
-  alias: {
-    '@': require('path').resolve(__dirname, 'src'), // src下的文件可以使用 @/xxx
-  },
-  nodeModulesTransform: {
-    type: 'none',
-  },
-  routes: routes,
-  fastRefresh: {}, // 快速刷新
-  hash: true, // 打包文件增加 hash 值，避免浏览器缓存
-  metas: [
-    {
-      name: 'keywords',
-      content: 'video',
+    antd: {},
+    dva: {},
+    routes: routes,
+    fastRefresh: {}, // 快速刷新
+    hash: true, // 打包文件增加 hash 值，避免浏览器缓存
+    devtool: false, // 关闭 sourcemap
+    alias: {
+        '@': require('path').resolve(__dirname, 'src'), // src下的文件可以使用 @/xxx
     },
-    {
-      name: 'description',
-      content: '资源丰富,高清好用的视频网站',
+    nodeModulesTransform: {
+        type: 'none',
     },
-  ],
+    metas: [
+        {
+            name: 'keywords',
+            content: 'video',
+        },
+        {
+            name: 'description',
+            content: '资源丰富,高清好用的视频网站',
+        },
+    ],
 });
