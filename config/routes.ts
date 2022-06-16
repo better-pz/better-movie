bfeb6625614c827254f73ec830de56eb2dcd9531
mfuu <446310957@qq.com>
2022-06-16 15:05:52 +0800

    Update routes.ts

--- config/routes.ts
+++ config/routes.ts
@@ -1,3 +1,10 @@
 export default [
-  { path: '/', component: '@/pages/index' },
+  {
+    path: '/',
+    component: '@/layouts/index',
+    routes: [
+      { path: '/', component: '@/pages/index' },
+      { component: '@/pages/404' }, // 不存在的路径直接跳转404页面
+    ],
+  },
 ]
